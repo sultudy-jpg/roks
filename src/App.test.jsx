c443b8230818d8c2c6cc9d82c97a542615c348ff
sultudy-jpg <sultudy@gmail.com>
2026-08-20 13:00:39 +0000

    Revert "Use official ROK Shipping logo"
    This reverts commit cf409ecba2f755968eb83e14b8da45e75ded68b6.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,8 +2,8 @@ import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders the ROK Shipping logo', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const logos = screen.getAllByRole('img', { name: /ROK Shipping Co\., Ltd\./i });
-  expect(logos).toHaveLength(2);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeDefined();
 });
